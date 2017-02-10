1b86cf6a3a2fc6a42c1ccf9230d21483267ba803
Derek Young <dthomasy@gmail.com>
2017-02-09 17:16:26 -0800

    Add socket.io

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const db = require('./server/db/database');
 const api = require('./server/api/api');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const port = process.env.PORT || 8000;
 
 require('./server/middleware')(app, express);
 
 app.use('/api', api);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 app.listen(port, function() {
   console.log('Listening on ' + port);
