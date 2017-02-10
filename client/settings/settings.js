5dbc6dfd8ea8406a3e2166e7f79e6f1e7af85250
Tenzin Sonam <tenso2006@gmail.com>
2017-02-10 15:03:23 -0800

    change: view setting

--- client/settings/settings.js
+++ client/settings/settings.js
@@ -1,10 +1,15 @@
 (function() {
   const SETTINGS = angular.module('zen.settings', []);
   SETTINGS.controller('SettingsCtrl', function($scope, ZenSetting) {
+
     $scope.update = function (day, startTime, endTime, breakType) {
       ZenSetting.postSetting(day, startTime, endTime, breakType);
     };
-    $scope.mode = ['edit', 'view'];
-    $scope.selection = $scope.mode[0];
+
+    ZenSetting.getSetting()
+    .then(function (data) {
+        $scope.settingData = data;
+        console.log('settingdata from controller ', $scope.settingData);
+    });
   });
 })();
