c2e59d75d76f96cf7596a952cfb9e5b562fd68d1
David Chang <dchang103@gmail.com>
2017-02-09 19:53:24 -0800

    this one should not be broken

--- client/app.js
+++ client/app.js
@@ -29,10 +29,6 @@
         templateUrl: '/home/home.html',
         controller: 'HomeCtrl'
       })
-      .when('/login', {
-        templateUrl: '/login/login.html',
-        controller: 'LoginCtrl'
-      })
       .when('/journey', {
         templateUrl: '/journey/journey.html',
         controller: 'JourneyCtrl'
