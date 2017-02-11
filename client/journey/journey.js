700d00890c2c09a318cefcfbe0ca829ed8392ca0
David Chang <dchang103@gmail.com>
2017-02-10 19:57:45 -0800

    graph empty table fix

--- client/journey/journey.js
+++ client/journey/journey.js
@@ -20,70 +20,71 @@
       headers: { user: $window.localStorage.user }
     })
     .then(function(data) {
-      console.log(arguments)
-      $scope.barchart.labels = [];
-      var labelReference = [];
-      var innerData = [];
+      if (data.length > 0) {
+        $scope.barchart.labels = [];
+        var labelReference = [];
+        var innerData = [];
 
-      var countDayBackwards = function(dateNum) {
-        var year = Math.floor(dateNum / 10000); 
-        var monthDate = (dateNum % 10000) - 1;
-        if (monthDate > 131 && monthDate < 201) {
-          monthDate = 131;
-        } else if (monthDate > 228 && monthDate < 301) {
-          monthDate = 228;
-        } else if (monthDate > 331 && monthDate < 401) {
-          monthDate = 331;
-        } else if (monthDate > 430 && monthDate < 501) {
-          monthDate = 430;
-        } else if (monthDate > 531 && monthDate < 601) {
-          monthDate = 531;
-        } else if (monthDate > 630 && monthDate < 701) {
-          monthDate = 630;
-        } else if (monthDate > 731 && monthDate < 801) {
-          monthDate = 731;
-        } else if (monthDate > 831 && monthDate < 901) {
-          monthDate = 831;
-        } else if (monthDate > 930 && monthDate < 1001) {
-          monthDate = 930;
-        } else if (monthDate > 1031 && monthDate < 1101) {
-          monthDate = 1031;
-        } else if (monthDate > 1130 && monthDate < 1201) {
-          monthDate = 1130;
+        var countDayBackwards = function(dateNum) {
+          var year = Math.floor(dateNum / 10000); 
+          var monthDate = (dateNum % 10000) - 1;
+          if (monthDate > 131 && monthDate < 201) {
+            monthDate = 131;
+          } else if (monthDate > 228 && monthDate < 301) {
+            monthDate = 228;
+          } else if (monthDate > 331 && monthDate < 401) {
+            monthDate = 331;
+          } else if (monthDate > 430 && monthDate < 501) {
+            monthDate = 430;
+          } else if (monthDate > 531 && monthDate < 601) {
+            monthDate = 531;
+          } else if (monthDate > 630 && monthDate < 701) {
+            monthDate = 630;
+          } else if (monthDate > 731 && monthDate < 801) {
+            monthDate = 731;
+          } else if (monthDate > 831 && monthDate < 901) {
+            monthDate = 831;
+          } else if (monthDate > 930 && monthDate < 1001) {
+            monthDate = 930;
+          } else if (monthDate > 1031 && monthDate < 1101) {
+            monthDate = 1031;
+          } else if (monthDate > 1130 && monthDate < 1201) {
+            monthDate = 1130;
+          }
+
+          if (monthDate.toString().length === 3) { monthDate = '' + 0 + monthDate; }
+
+          return '' + year + monthDate;
+        };
+
+        var findReps = function(dateNum) {
+          for (let i = 0; i < data.length; i++) {
+            if (data[i].date == dateNum) { return data[i].reps; }
+          }
+        };
+
+        var recentDate = data[data.length - 1].date;
+        labelReference.unshift(recentDate);
+        var currentDate = recentDate;
+
+        while (labelReference.length < 7) {
+          currentDate = countDayBackwards(currentDate);
+          labelReference.unshift(currentDate);
         }
 
-        if (monthDate.toString().length === 3) { monthDate = '' + 0 + monthDate; }
-
-        return '' + year + monthDate;
-      };
-
-      var findReps = function(dateNum) {
-        for (let i = 0; i < data.length; i++) {
-          if (data[i].date == dateNum) { return data[i].reps; }
+        for (let i = 0; i < labelReference.length; i++) {
+          $scope.barchart.labels.push(labelReference[i].slice(4, 6)  + '/' + labelReference[i].slice(6));
+          var reps = findReps(labelReference[i]);
+          if (!reps) { 
+            innerData.push(0); 
+          } else {
+            innerData.push(reps);
+          }
         }
-      };
 
-      var recentDate = data[data.length - 1].date;
-      labelReference.unshift(recentDate);
-      var currentDate = recentDate;
-
-      while (labelReference.length < 7) {
-        currentDate = countDayBackwards(currentDate);
-        labelReference.unshift(currentDate);
+        $scope.barchart.data.push(innerData);
+        $timeout(function() {}, 0); //makes this bullshit render
       }
-
-      for (let i = 0; i < labelReference.length; i++) {
-        $scope.barchart.labels.push(labelReference[i].slice(4, 6)  + '/' + labelReference[i].slice(6));
-        var reps = findReps(labelReference[i]);
-        if (!reps) { 
-          innerData.push(0); 
-        } else {
-          innerData.push(reps);
-        }
-      }
-
-      $scope.barchart.data.push(innerData);
-      $timeout(function() {}, 0); //makes this bullshit render
     });
   });
 })();
