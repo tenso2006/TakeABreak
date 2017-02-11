e335d88cb04bfc8ae7602600695469d128191a28
Tenzin Sonam <tenso2006@gmail.com>
2017-02-10 20:59:24 -0800

    fix

--- client/settings/settings.js
+++ client/settings/settings.js
@@ -1,15 +1,18 @@
 (function() {
   const SETTINGS = angular.module('zen.settings', []);
 
-  SETTINGS.controller('SettingsCtrl', function($scope, Api) {
+  SETTINGS.controller('SettingsCtrl', function($scope, Api, $timeout) {
 
     $scope.update = function (day, startTime, endTime, breakType) {
       Api.postSetting(day, startTime, endTime, breakType);
     };
 
-    Api.getSetting().then(function (data) {
-      $scope.settingData = data;
-      console.log('settingdata from controller ', $scope.settingData);
-    });
+    $scope.get = function () {
+      Api.getSetting().then(function (data) {
+        $scope.settingData = data;
+        $timeout(function (){}, 0);
+        console.log('settingdata from controller ', $scope.settingData);
+      });
+    };
   });
 })();
