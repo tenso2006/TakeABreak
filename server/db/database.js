152abf50b8565fb459a32e88e5870037b4aec7da
Derek Young <dthomasy@gmail.com>
2017-02-11 18:59:01 -0800

    Prod DB

--- server/db/database.js
+++ server/db/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const autoIncrement = require('mongoose-auto-increment');
-const db = mongoose.connect('mongodb://localhost/takeABreak');
+// const db = mongoose.connect('mongodb://localhost/takeABreak'); // Dev
+const db = mongoose.connect('mongodb://ds019886.mlab.com:19886/heroku_s4t93j7w'); // Prod
 autoIncrement.initialize(db);
 
 module.exports = db;
